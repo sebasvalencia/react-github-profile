25a4283b921b2d240821d04f975a6939fdf32b05
Sebastian <sebastian.valencian@gmail.com>
2019-05-21 22:10:22 -0500

    [Feature]Create state in App and createUser method createUser on UserFrm

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import "./App.css";
 import UserForm from "./components/UserForm";
 
 class App extends React.Component {
+  state = {
+    user: {}
+  };
+
+  createUser = user => {
+    console.log("createUser App", user);
+    this.setState({
+      user
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">GITHUB PROFILE</header>
-        <UserForm />
+        <UserForm createUser={this.createUser} />
       </div>
     );
   }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,19 +1,38 @@
 import React from "react";
 
 class UserForm extends React.Component {
-  createUser(event) {
+
+
+    nameRef = React.createRef();
+    idRef = React.createRef();
+    emailRef = React.createRef();
+    birthdayRef = React.createRef();
+    githubRef = React.createRef();
+
+
+  createUser = (event) => {
+      console.log("create User");
       event.preventDefault();
-    console.log("create User");
+      const user = {
+        name : this.nameRef.current.value,
+        id: this.idRef.current.value,
+        email: this.emailRef.current.value,
+        birthday: this.birthdayRef.current.value,
+        githubRef: this.githubRef.current.value
+      }
+      console.log(user);
+
+      this.props.createUser(user);
   }
 
   render() {
     return (
       <form className="user-form" onSubmit={this.createUser}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="id" placeholder="Identification" />
-        <input type="text" name="email" placeholder="Email" />
-        <input type="text" name="birthday" placeholder="Birthday" />
-        <input type="text" name="github" placeholder="Github User" />
+        <input type="text" name="name" placeholder="Name" ref={this.nameRef} />
+        <input type="text" name="id" placeholder="Identification" ref={this.idRef} />
+        <input type="text" name="email" placeholder="Email" ref={this.emailRef} />
+        <input type="text" name="birthday" placeholder="Birthday" ref={this.birthdayRef} />
+        <input type="text" name="github" placeholder="Github User" ref={this.githubRef} />
         <button type="submit">Create</button>
       </form>
     );
